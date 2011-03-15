da118fb80705a26a13c6f0475a262c98adbb0795
Justin Wishart <jwwishart@example.com>
2011-03-15 20:22:27 +1000

    finished desired functionaliry

--- machen.js
+++ machen.js
@@ -1,16 +1,38 @@
 (function(window, $) {
 	var machen = {
-		setupViewport : function() {	
+		setupViewport : function(fullScreenMode) {
+			if (undefined == fullScreenMode)
+				fullScreenMode = false;
+				
 			var header = $("#header");
 			var content = $("#content");
 			var footer = $("#footer");
+			var pageNumber = $("#page_number");
+			var navigation = $("#navigation");
+			
+			if (true == fullScreenMode) {
+				header.fadeOut();
+				footer.fadeOut();
+				pageNumber.fadeOut();
+				navigation.fadeOut();
+			} else {
+				header.fadeIn();
+				footer.fadeIn();
+				pageNumber.fadeIn();
+				navigation.fadeIn();
+			}
 			
 			// Calculate the height of the content area
 			var totalHeight = $(window).height();
-			totalHeight -= header.outerHeight(true);
-			totalHeight -= footer.outerHeight(true);
+			
+			if (false == fullScreenMode) {
+				totalHeight -= header.outerHeight(true);
+				totalHeight -= footer.outerHeight(true);
+			}
+			
 			totalHeight -= parseInt(content.css("padding-top").replace("px", ""))
-			             + parseInt(content.css("padding-bottom").replace("px", ""));
+							 + parseInt(content.css("padding-bottom").replace("px", ""));
+							 
 			content.height(totalHeight);
 			
 			// Calcultae the width of the content area
@@ -21,7 +43,7 @@
 		}
 		,
 		lastPanelID : 0,
-		currentPanelID : 0,		
+		currentPanelID : 0,	
 		next : function() {
 			// See if there is an answer panel inside the current panel and show it.
 			var answerSection = this.getCurrentPanel().find(".machen-answer").filter(":hidden");
@@ -32,34 +54,67 @@
 				answerSection.show();				
 		}
 		,
+		showNextAnswer : function(){
+			var allAnswers = this.getCurrentPanel().find(".machen-answer");
+			
+		}
+		,
 		back : function() {
 			this.showPanel( this.currentPanelID == 0 ? 0 : this.currentPanelID - 1);
 		}
 		,
 		showPanel : function(panelNo) {
-			$(".machen-panel").hide();
+			var allPanels = this.getAllPanels()
+			allPanels.hide();
+			
 			$(".machen-panel[data-panelid='" + panelNo +"']").show();
+			
 			this.currentPanelID = panelNo;
 			this.getCurrentPanel().find(".machen-answer").hide();
 			
-			$("#page_number"). html("Page " + parseInt(panelNo + 1));
+			$("#page_number").html("Page " + parseInt(panelNo + 1));
 		}
 		,
 		getLastPanel : function() {
-			return $(".machen-panel").last().attr("data-panelid");
+			return this.getAllPanels().last();
+		}
+		,
+		getPanelByID : function(panelNo) {
+			return $(".machen-panel[data-panelid='" + panelNo +"']");
 		}
 		,
 		getCurrentPanel : function() {
-			return $(".machen-panel[data-panelid=" + this.currentPanelID + "]");
+			return this.getPanelByID(this.currentPanelID);
 		}
 		,
 		getAllPanels : function() {
 			return $(".machen-panel");
 		}
+		,
+		switchModes : function() {
+			var heading = $("#header");
+			this.setupViewport(heading.is(":visible"));
+		}
 	};
+	
+	
+	function setupPanels() {
+		machen.getAllPanels().each(function(i, el) {
+			$(el).attr("data-panelid", i.toString());
+		});
+	}
 	
 	// Ensure we setup the viewport initially.
 	$(window.document).ready(function() {
+		var header = $("#header");
+		var content = $("#content");
+		var footer = $("#footer");
+		var pageNumber = $("#page_number");
+		var navigation = $("#navigation");
+			
+		// Assing ID's
+		setupPanels();
+	
 		// Setup nice viewport stuff 
 		machen.setupViewport();
 		
@@ -74,32 +129,55 @@
 		machen.setupViewport();
 		
 		// Count the panels
-		machen.lastPanelID = machen.getLastPanel();
+		machen.lastPanelID = machen.getLastPanel().attr("data-panelid");
 		
 		// Hide all panels and show the first one
 		machen.getAllPanels().find(".machen-answer").hide();
 		machen.showPanel(0);
 		
-		// NOTE: Have to use keydown as chrome doesn't capture backspace
+		// NOTE: Have to use keydown event as chrome doesn't capture backspace
 		$(window.document).keydown(function(event) {
 			//alert(event.which);
 			
-			// 32 is the space key; 13 is the enter key
+			// 32 is the space key; 13 is the enter key; 39 is the right arrow key
 			if ('32' == event.which || '13' == event.which || '39' == event.which ) {
 				event.preventDefault();
 				machen.next();
 			}
 			
-			// 8 is the backspace key
+			// 8 is the backspace key - 37 is left arrow key
 			if ('8' == event.which || '37' == event.which) {
 				event.preventDefault();
 				machen.back();
 			}
+			
+			// Alternate from full screen mode and normal mode
+			if ('70' == event.which) {
+				machen.switchModes();
+			}
 		});
 		
-		var content = $("#content");
+		// Handle someone clicking on the content area, which should
+		// cause the next panel to be shown.
+		// Note: selection must be disabled to prevent massive content
+		// selection.
 		content.mouseup(function() {
 			machen.next();
+		});
+		
+		navigation.fadeTo("fast", 0.25);
+		
+		navigation.hover(function(){
+			navigation.fadeTo("slow", 1);
+		}, function() { 
+			navigation.fadeTo("slow", 0.25);
+		});
+		
+		footer.fadeTo("fast", 0.25);
+		footer.hover(function() {
+			footer.fadeTo("slow", 1);
+		}, function() {
+			footer.fadeTo("slow", 0.25);
 		});
 	});
 	
